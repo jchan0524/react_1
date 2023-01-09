1a08cc46a70a16cb9ad8158bdff4e9ada58d9a0d
jchan0524 <jchan@udel.edu>
2023-01-09 12:43:03 -0500

    Start Routing

--- src/App.js
+++ src/App.js
@@ -4,92 +4,22 @@ import './index.css';
 import Employee from './components/Employee'; 
 import {useState} from 'react'; 
 import {v4 as uuidv4} from 'uuid'; 
+import AddEmployee from './components/AddEmployee';
+import EditEmployee from './components/EditEmployee';
+import Header from './components/Header'; 
+import Employees from './pages/Employees'; 
 
 function App() {
   
-  const [role, setRole] = useState('dev'); 
-  const [employees, setEmployees] = useState(
-    [
-      {
-        name: "Abby", 
-      role:"Developer", 
-      img: "https://images.pexels.com/photos/14711370/pexels-photo-14711370.jpeg"
-    },
-    { 
-      name: "Jane", 
-    role:"Developer", 
-    img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg"
-  },
-  { 
-    name: "Caleb", 
-  role:"Developer", 
-  img: "https://images.pexels.com/photos/4890733/pexels-photo-4890733.jpeg"
-},
-{ 
-  name: "John", 
-role:"Engineer", 
-img: "https://images.pexels.com/photos/2741701/pexels-photo-2741701.jpeg"
-},
-{ 
-  name: "Akiva", 
-role:"Sitting", 
-img: "https://images.pexels.com/photos/2287252/pexels-photo-2287252.jpeg"
-},
-{ 
-  name: "Rob", 
-role:"Podcasting", 
-img: "https://images.pexels.com/photos/2104252/pexels-photo-2104252.jpeg"
-},
-
-    ]); 
-  //Can put statements before the return - variables, printing 
-  const showEmployees = true; 
-  return (
-    <div className="App">
-      {/* to show js code must use curly brackets before code */}
-      {/* Is blank true use question mark */}
-      {showEmployees ? ( 
-      
-        <>
-        <input 
-        type = 'text' 
-        onChange={
-          (e) => {console.log(e.target.value); 
-            // Use state!!!
-            // if it starts with use it is most likely a hook
-            setRole(e.target.value); 
-        }}
-        />
-      
-
-        <div className = "flex flex-wrap justify-center">
-          {employees.map((employees)=>{
+  
+          
             
-            return(
-            <Employee 
-              key = {uuidv4()}
-              name={employees.name} 
-              role={employees.role} 
-              img={employees.img}
-              
-              />
-            );  
-          }
-
-          )}
-        
-        </div>
-        </>
-        ):(
-      
-        <p>
-         You cannot see the employees 
-        </p>
-      )}
-     
-      
-    </div>
-  );
+            return (
+              <Header>
+                <Employees />
+              </Header>
+            
+            );
 }
 
 export default App;
---
+++ src/pages/Employees.js
@@ -0,0 +1,136 @@
+
+import React from 'react';
+import '../index.css';
+import Employee from '../components/Employee'; 
+import {useState} from 'react'; 
+import {v4 as uuidv4} from 'uuid'; 
+import AddEmployee from '../components/AddEmployee';
+import EditEmployee from '../components/EditEmployee';
+import Header from '../components/Header'; 
+
+
+
+function Employees() {
+  
+  const [role, setRole] = useState('dev'); 
+  const [employees, setEmployees] = useState(
+    [
+      {
+        id : 1,
+        name: "Abby", 
+      role:"Developer", 
+      img: "https://images.pexels.com/photos/14711370/pexels-photo-14711370.jpeg"
+    },
+    { 
+      id : 2,
+      name: "Jane", 
+    role:"Developer", 
+    img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg"
+  },
+  { 
+    id : 3,
+    name: "Caleb", 
+  role:"Developer", 
+  img: "https://images.pexels.com/photos/4890733/pexels-photo-4890733.jpeg"
+},
+{ 
+  id : 4,
+  name: "John", 
+role:"Engineer", 
+img: "https://images.pexels.com/photos/2741701/pexels-photo-2741701.jpeg"
+},
+{ 
+  id : 5,
+  name: "Akiva", 
+role:"Sitting", 
+img: "https://images.pexels.com/photos/2287252/pexels-photo-2287252.jpeg"
+},
+{ 
+  id : 6,
+  name: "Rob", 
+role:"Podcasting", 
+img: "https://images.pexels.com/photos/2104252/pexels-photo-2104252.jpeg"
+},
+
+    ]);
+    function updateEmployee(id, newName, newRole){
+      const updatedEmployees = employees.map((employee)=>{
+        if(id===employee.id){
+          return{...employee, name: newName, role: newRole}
+        }
+        return employee; 
+      }); 
+      setEmployees(updatedEmployees); 
+    } 
+
+    function newEmployee(name, role, img){
+      const newEmployee = {
+        id: uuidv4(),
+        name: name,
+        role: role, 
+        img: img,
+      };
+      setEmployees([...employees, newEmployee])
+
+
+    }
+
+  //Can put statements before the return - variables, printing 
+  const showEmployees = true; 
+  return (
+    <div className="App bg-gray-300 min-h-screen">
+      
+      {/* to show js code must use curly brackets before code */}
+      {/* Is blank true use question mark */}
+      {showEmployees ? ( 
+      
+        <>
+       
+        
+      
+
+        <div className = "flex flex-wrap justify-center">
+          {employees.map((employee)=>{
+            const editEmployee = (
+            <EditEmployee 
+            id={employee.id}
+            name={employee.name} 
+            role={employee.role}
+            updateEmployee={updateEmployee}
+             />
+
+            ); 
+          
+            
+            return (
+            <Employee 
+              key = {employee.id}
+              id={employee.id}
+              name={employee.name} 
+              role={employee.role} 
+              img={employee.img}
+              editEmployee = {editEmployee}
+              
+              />
+            );  
+          }
+
+          )}
+        
+        </div>
+        <AddEmployee newEmployee ={newEmployee} 
+        />
+        </>
+        ):(
+      
+        <p>
+         You cannot see the employees 
+        </p>
+      )}
+     
+      
+    </div>
+  );
+}
+
+export default Employees;
