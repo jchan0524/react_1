decb49b13de21ee737e1e12c63a319d2fc71953f
jchan0524 <jchan@udel.edu>
2023-01-13 19:36:11 -0500

    last commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { LoginContext } from "../App";
 
